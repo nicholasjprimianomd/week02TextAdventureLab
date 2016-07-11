9db2e7972a5b5c29708b875e1d430284c2a8a6e5
Nicholas Primiano <njp280@nyu.edu>
2016-07-11 18:14:36 -0400

    no message

--- Assets/Scripts/NewText.cs
+++ Assets/Scripts/NewText.cs
@@ -1,11 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System.Security.Policy;
 
 public class NewText : MonoBehaviour {
 
-	public Text myTextThing;
-	int currentPoints = 0 ;
+	string currentRoom = "Lobby";
 
 	// Use this for initialization
 	void Start () {
@@ -14,13 +14,39 @@ public class NewText : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		//give player 1 point if they press Space
-		if(Input.GetKeyDown(KeyCode.Space)){
-			currentPoints++;
-			myTextThing.text = "Current Score: " + currentPoints.ToString ();
+
+		string textBuffer = "You are currently in: " + currentRoom;
+		bool hasStudentID = false;
+
+		if(currentRoom == "Lobby"){
+			textBuffer += "\nYou see the security gaurd.";
+			textBuffer += "\nPress [W] to go to the elevators";
+			textBuffer += "\nPress [S] to go outside.";
+			if(Input.GetKeyDown(KeyCode.W)){
+					currentRoom = "Elevators";
+				}
+				else if (Input.GetKeyDown (KeyCode.S)){
+					currentRoom = "Outside";	
+				}
+		} else if (currentRoom == "Elevators"){
+			textBuffer += "\nYou are waiting";
+			if (hasStudentID == false){
+				textBuffer += "\nYou can't go in without your ID car, though...";
+			}	
+			else{
+				textBuffer += "\nYou swipe your student ID and the gaur smiles.";
+			}
+			//TODO: add choice to take elevator up to the classroom?
+		} else if (currentRoom == "Outside"){
+			textBuffer += "\nIT IS REALY HOT WHAT IS WRONG WITH YOU";
+			textBuffer += "\npress [S] to go back INSIDE,LIKE RIGHT NOW";
+			textBuffer += "\n(oh hey you found your student IF on the floor!)";
+			hasStudentID = true;
+			if (Input.GetKeyDown (KeyCode.S)){
+				currentRoom = "Lobby";
+			}
 		}
-		if(currentPoints >= 10){
-			myTextThing.text = "You Win!";
-		}
+		GetComponent<Text> ().text = textBuffer;
+
 	}
 }
