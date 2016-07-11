5c1ee87ec42eb7e132d3d6cecf3947dd8e56848e
Nicholas Primiano <njp280@nyu.edu>
2016-07-11 15:41:46 -0400

    no message

---
+++ Assets/Scripts/NewText.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NewText : MonoBehaviour {
+
+	public Text myTextThing;
+	int currentPoints = 0 ;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//give player 1 point if they press Space
+		if(Input.GetKeyDown(KeyCode.Space)){
+			currentPoints++;
+			myTextThing.text = "Current Score: " + currentPoints.ToString ();
+		}
+		if(currentPoints >= 10){
+			myTextThing.text = "You Win!";
+		}
+	}
+}
